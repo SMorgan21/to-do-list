478609a653b0ab99743343a89379a37a51a586a8
simonmorgan <simon.morgan@superiorwellness.co.uk>
2024-06-11 10:36:42 +0100

    Removed local server settings from vite config

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            protocol: 'ws',
-            https: false,
-            host: '192.168.56.56',
-        },
-        host: "192.168.56.56",
-        https: false,
-        watch: {
-            usePolling: true,
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
